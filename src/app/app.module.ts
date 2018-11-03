06b052afe6f006ea9f60a7e836a43bf4cc6ba7e0
Dennis Mullen <dkmullen@gmail.com>
2018-11-03 12:07:30 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MatButtonModule, MatCheckboxModule, MatTabsModule } from '@angular/material';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppComponent } from './app.component';
+import { PageoneComponent } from './pageone/pageone.component';
+import { PagetwoComponent } from './pagetwo/pagetwo.component';
+import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PageoneComponent },
+  { path: 'p2', component: PagetwoComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageoneComponent,
+    PagetwoComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    NgxDatatableModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pagetwo/pagetwo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagetwo',
+  templateUrl: './pagetwo.component.html',
+  styleUrls: ['./pagetwo.component.css']
+})
+export class PagetwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+  ];
+  columns = [
+    { prop: 'name' },
+    { name: 'Gender' },
+    { name: 'Company' }
+  ];
+
+}
